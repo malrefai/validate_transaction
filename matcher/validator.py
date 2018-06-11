9599c53503de0c7a59894c507823d82746f8493e
Mohammad Alrefai <malrefai@nyit.edu>
2018-06-11 19:20:18 +0300

    Remove error parameter and fix raising exceptions

--- matcher/validator.py
+++ matcher/validator.py
@@ -7,7 +7,7 @@ validator.py
 """
 
 
-from .error import SchemaMissingKeyError, SchemaUnexpectedTypeError, DataUnmatchedTypeError
+from .error import SchemaMissingKeyError, SchemaUnexpectedTypeError, SchemaUnrecognizableTypeError
 from .type import CUSTOM_TYPES, Type
 
 __version__ = '0.1.0'
@@ -36,26 +36,26 @@ class Validator:
         return Type.factory(data_type.strip().lower()).match(data)
 
     @staticmethod
-    def check_structure(schema, data, error=None):
+    def check_structure(schema, data):
 
         if Validator._is_dict(schema, data):
             for key in schema:
                 if key not in data:
                     raise SchemaMissingKeyError("Missing key: %s" % key)
-                Validator.check_structure(schema[key], data[key], error)
+                Validator.check_structure(schema[key], data[key])
 
         elif Validator._is_list(schema, data):
             # schema  is list in the form [type or dict]
             for datum in data:
-                Validator.check_structure(schema[0], datum, error)
+                Validator.check_structure(schema[0], datum)
 
         elif Validator._is_type(schema):
             # structure is the type of data
             if not Validator._is_data_match_type(schema, data):
-                raise DataUnmatchedTypeError(
-                    "Mismatch data with type: '%s' is not instance of %s" % (data, schema), error)
+                raise SchemaUnexpectedTypeError(
+                    "Mismatch data with type: '%s' is not instance of %s" % (data, schema))
 
         else:
             # structure is neither a dict, nor list, not TYPES
-            raise SchemaUnexpectedTypeError("Unrecognizable type in schema: %s" % schema, error)
+            raise SchemaUnrecognizableTypeError("Unrecognizable type in schema: %s" % schema)
 
